c246ea361187526c790178b655c6fa9a7d59cc32
ev-indrik <delfinmur@gmail.com>
2025-05-30 21:37:06 +0300

    feat: Antd tokens applied

--- src/components/app-layout/AppLayout.tsx
+++ src/components/app-layout/AppLayout.tsx
@@ -1,11 +1,12 @@
 import './AppLayout.scss'
-import {notification, Row} from "antd";
+import {ConfigProvider, Row} from "antd";
 
 import CalculatingSection from "../calculation-section/CalculatingSection.tsx";
 import ResultSection from "../result-section/ResultSection.tsx";
 import {defaultValues, GeneralContext} from '../../context/context.tsx';
 import {useState} from "react";
-import { NotificationProvider } from '../../context/notificationContext.tsx';
+import {NotificationProvider} from '../../context/notificationContext.tsx';
+import {theme} from "../../theme/theme.ts";
 
 function AppLayout() {
 
@@ -18,14 +19,18 @@ function AppLayout() {
                     calculationResults,
                     setCalculationResults
                 }}>
-                <div className={'app-wrapper'}>
-                    <div className={'container'}>
-                        <Row justify={'center'} className={'calc-wrapper'}>
-                            <CalculatingSection/>
-                            <ResultSection/>
-                        </Row>
+                <ConfigProvider
+                    theme={theme}
+                >
+                    <div className={'app-wrapper'}>
+                        <div className={'container'}>
+                            <Row justify={'center'} className={'calc-wrapper'}>
+                                <CalculatingSection/>
+                                <ResultSection/>
+                            </Row>
+                        </div>
                     </div>
-                </div>
+                </ConfigProvider>
             </GeneralContext.Provider>
         </NotificationProvider>
     )
